edf449b549245ea6f5883beb8eebd3e853c43464
rolemberg <vinnie.nsts@gmail.com>
2024-04-26 18:29:03 -0300

    initing cors config;

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -3,8 +3,10 @@ const jwt = require('jsonwebtoken');
 exports.login = (req, res) => {
    if(req.body.user == process.env.LOGUSER && req.body.password == process.env.PASS) {
       const token = jwt.sign({ user_id: process.env.USERID }, process.env.SEC, { expiresIn: '10m' });
+
       req.session.user_id = process.env.USERID;
       req.session.token = token;
+      
       return res.status(200).json({ user_id: req.session.user_id, loged: true, token }).end();
    };
 
--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -2,9 +2,8 @@ const jwt = require('jsonwebtoken');
 
 exports.jwtAuth = (req, res, next) => {
    req.headers['x-access-token'] = req.session.token;
-   const token = req.session.token;
 
-   jwt.verify(token, process.env.SEC, (err, decoded) => {
+   jwt.verify(req.headers['x-access-token'], process.env.SEC, (err, decoded) => {
       if(err) return res.status(401).json({ status: 'invalid token' }).end();
       req.body.user_id = decoded.user_id;
       next();
